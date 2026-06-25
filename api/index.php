badc00423b8007c04759279921f3bad648992000
azkiaputri2907 <puteee63@gmail.com>
2026-06-25 13:04:56 +0800

    Fix vendor mapping and switch back to secure bridge index

--- api/index.php
+++ api/index.php
@@ -1,20 +1,4 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Mengunci base path agar sesuai dengan lingkungan Vercel Linux
-$app = require __DIR__ . '/../bootstrap/app.php';
-
-// Memaksa Laravel mengenali jalur folder views dan resources di Vercel
-$app->useStoragePath('/tmp');
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// Mengarahkan Vercel serverless ke file core Laravel
+require __DIR__ . '/../public/index.php';
