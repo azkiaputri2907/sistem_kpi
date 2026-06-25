b59e6c4463d8f68dae9fe956c5373224196cf88e
azkiaputri2907 <puteee63@gmail.com>
2026-06-25 12:47:41 +0800

    Fix local node modules and build assets

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 
 // Jembatan untuk mengalihkan traffic Vercel Serverless ke public/index.php bawaan Laravel
-require __DIR__ . '/public/index.php';
+require __DIR__ . '/../public/index.php';
